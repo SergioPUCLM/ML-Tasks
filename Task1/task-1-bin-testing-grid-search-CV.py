73efced428b7654b932f67f01e0aef9f88946098
Sergio Crafter <sergio.pozuelo4@alu.uclm.es>
2024-10-24 23:18:44 +0200

    Increased bin range, fixed issue where bins were only ever calculated as 3, reduced cv in cross validation from 10 to 8

--- Task1/task-1-bin-testing-grid-search-CV.py
+++ Task1/task-1-bin-testing-grid-search-CV.py
@@ -28,20 +28,23 @@ def main():
     mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
     mlflow.set_experiment("WINE QUALITY")
 
-
     # Fetch dataset
     print("Fetching dataset...")
     wine_quality = fetch_ucirepo(id=186)
 
     # Data (as pandas dataframes)
     print("Data fetched successfully, formatting data...")
-    x = wine_quality.data.features
+    X = wine_quality.data.features
     y = wine_quality.data.targets
+
+    # Remove useless features
+    x = X.drop(columns=['total_sulfur_dioxide', 'density'])
 
     feature_names = x.columns.tolist()
 
-    MIN_BINS = 5 
-    MAX_BINS = 15
+    MIN_BINS = 5
+    MAX_BINS = 30
+    # 25 Possible bin values (10 takes ~30 mins, 25 should be about 1h 15mins)
     SLEEP_TIME = 0
 
 
@@ -78,7 +81,7 @@ def main():
         ## RANDOM FOREST
         print("Running Random Forest Classifier...")
         model = RandomForestClassifier(n_estimators=100, random_state=42)
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=10, n_jobs=-1)
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=8, n_jobs=-1)
         grid_search.fit(x_train, y_train)
         
         best_params = grid_search.best_params_
@@ -86,15 +89,15 @@ def main():
 
         y_pred = grid_search.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred, output_dict=True)
+        report = classification_report(y_test, y_pred, output_dict=True, zero_division=0)
 
-        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Random Forest Classifier Bins {bins}", f"random-forest-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test)
+        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Random Forest Classifier Bins {bins}", f"random-forest-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test, grid_search=grid_search)
         time.sleep(SLEEP_TIME)
 
         ## GRADIENT BOOSTING
         print("Running Gradient Boosting Classifier...")
         model = GradientBoostingClassifier(n_estimators=100, random_state=42)
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=10, n_jobs=-1)
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=8, n_jobs=-1)
         grid_search.fit(x_train, y_train)
 
         best_params = grid_search.best_params_
@@ -102,15 +105,15 @@ def main():
 
         y_pred = grid_search.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred, output_dict=True)
+        report = classification_report(y_test, y_pred, output_dict=True, zero_division=0)
 
-        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Gradient Boosting Classifier Bins {bins}", f"gradient-boosting-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test)
+        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Gradient Boosting Classifier Bins {bins}", f"gradient-boosting-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test, grid_search=grid_search)
         time.sleep(SLEEP_TIME)
 
         ## SUPPORT VECTOR MACHINE
         print("Running Support Vector Machine Classifier...")
         model = SVC(kernel='linear', random_state=42)
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_svc, cv=10, n_jobs=-1)
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_svc, cv=8, n_jobs=-1)
         grid_search.fit(x_train, y_train)
 
         best_params = grid_search.best_params_
@@ -118,15 +121,15 @@ def main():
 
         y_pred = grid_search.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred, output_dict=True)
+        report = classification_report(y_test, y_pred, output_dict=True, zero_division=0)
 
-        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Support Vector Machine Classifier Bins {bins}", f"support-vector-machine-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test)
+        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Support Vector Machine Classifier Bins {bins}", f"support-vector-machine-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test, grid_search=grid_search)
         time.sleep(SLEEP_TIME)
 
         ## K-NEAREST NEIGHBORS
         print("Running K-Nearest Neighbors Classifier...")
         model = KNeighborsClassifier(n_neighbors=3)
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_k, cv=10, n_jobs=-1)
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_k, cv=8, n_jobs=-1)
         grid_search.fit(x_train, y_train)
 
         best_params = grid_search.best_params_
@@ -134,15 +137,15 @@ def main():
 
         y_pred = grid_search.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred, output_dict=True)
+        report = classification_report(y_test, y_pred, output_dict=True, zero_division=0)
 
-        mlflow_run(accuracy, report, x_train, best_model, best_params, f"K-Nearest Neighbors Classifier Bins {bins}", f"k-nearest-neighbors-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test)
+        mlflow_run(accuracy, report, x_train, best_model, best_params, f"K-Nearest Neighbors Classifier Bins {bins}", f"k-nearest-neighbors-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test, grid_search=grid_search)
         time.sleep(SLEEP_TIME)
 
         ## GAUSSIAN NAIVE BAYES
         print("Running Gaussian Naive Bayes Classifier...")
         model = GaussianNB()
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_nb_gaussian, cv=10, n_jobs=-1)
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_nb_gaussian, cv=8, n_jobs=-1)
         grid_search.fit(x_train, y_train)
 
         best_params = grid_search.best_params_
@@ -150,15 +153,15 @@ def main():
 
         y_pred = grid_search.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred, output_dict=True)
+        report = classification_report(y_test, y_pred, output_dict=True, zero_division=0)
 
-        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Gaussian Naive Bayes Classifier Bins {bins}", f"gaussian-naive-bayes-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test)
+        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Gaussian Naive Bayes Classifier Bins {bins}", f"gaussian-naive-bayes-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test, grid_search=grid_search)
         time.sleep(SLEEP_TIME)
 
         ## MULTINOMIAL NAIVE BAYES
         print("Running Multinomial Naive Bayes Classifier...")
         model = MultinomialNB()
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_nb, cv=10, n_jobs=-1)
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_nb, cv=8, n_jobs=-1)
         grid_search.fit(x_train, y_train)
 
         best_params = grid_search.best_params_
@@ -166,15 +169,15 @@ def main():
 
         y_pred = grid_search.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred, output_dict=True)
+        report = classification_report(y_test, y_pred, output_dict=True, zero_division=0)
 
-        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Multinomial Naive Bayes Classifier Bins {bins}", f"multinomial-naive-bayes-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test)
+        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Multinomial Naive Bayes Classifier Bins {bins}", f"multinomial-naive-bayes-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test, grid_search=grid_search)
         time.sleep(SLEEP_TIME)
 
         ## COMPLEMENT NAIVE BAYES
         print("Running Complement Naive Bayes Classifier...")
         model = ComplementNB()
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_nb, cv=10, n_jobs=-1)
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid_nb, cv=8, n_jobs=-1)
         grid_search.fit(x_train, y_train)
         
         best_params = grid_search.best_params_
@@ -182,13 +185,13 @@ def main():
 
         y_pred = grid_search.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred, output_dict=True)
+        report = classification_report(y_test, y_pred, output_dict=True, zero_division=0)
 
-        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Complement Naive Bayes Classifier Bins {bins}", f"complement-naive-bayes-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test)
+        mlflow_run(accuracy, report, x_train, best_model, best_params, f"Complement Naive Bayes Classifier Bins {bins}", f"complement-naive-bayes-classifier-bins-{bins}", feature_names, x_test=x_test, y_test=y_test, grid_search=grid_search)
         time.sleep(SLEEP_TIME)
 
 
-def mlflow_run(accuracy, report, x_train, model, best_params, model_name, artifact_path="wine_quality_model", feature_names=None, x_test=None, y_test=None):
+def mlflow_run(accuracy, report, x_train, model, best_params, model_name, artifact_path="wine_quality_model", feature_names=None, x_test=None, y_test=None, grid_search=None):
     print("Logging metrics and model...")
     with mlflow.start_run():
         mlflow.log_params(best_params)
@@ -196,6 +199,7 @@ def mlflow_run(accuracy, report, x_train, model, best_params, model_name, artifa
         mlflow.log_metric("precision", report['weighted avg']['precision'])
         mlflow.log_metric("recall", report['weighted avg']['recall'])
         mlflow.log_metric("f1", report['weighted avg']['f1-score'])
+        mlflow.log_metric("Best cross-validation score", grid_search.best_score_)
         mlflow.set_tag("Training Info", model_name)
 
         # Log feature importance or coefficients if available
@@ -228,10 +232,10 @@ def mlflow_run(accuracy, report, x_train, model, best_params, model_name, artifa
 def discretize_data(bins, x, y):
     # Discretize the data
     print("Discretizing data...")
-    discretizer = KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='quantile')
+    discretizer = KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='uniform')
     X_disc = discretizer.fit_transform(x)
     y_disc = discretizer.fit_transform(y)
-    x_train, x_test, y_train, y_test = train_test_split(X_disc, y_disc, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(X_disc, y_disc.flatten(), test_size=0.2, random_state=42)
     return x_train, x_test, y_train, y_test
 
 
